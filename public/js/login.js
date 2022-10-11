377e2440ac9af184ef88da08ec9414e06ea05ee5
Headwall Forest <headwallforest27@gmail.com>
2022-10-11 19:04:52 -0400

    Two seperate functional pages for login and signup achieved

--- public/js/login.js
+++ public/js/login.js
@@ -22,31 +22,8 @@ const loginFormHandler = async (event) => {
   }
 };
 
-const signupFormHandler = async (event) => {
-  event.preventDefault();
-
-  const user_name = document.querySelector('#user-signup').value.trim();
-  const password = document.querySelector('#password-signup').value.trim();
-
-  if (user_name && password) {
-    const response = await fetch('/api/users', {
-      method: 'POST',
-      body: JSON.stringify({ user_name, password }),
-      headers: { 'Content-Type': 'application/json' },
-    });
-
-    if (response.ok) {
-      document.location.replace('/profile');
-    } else {
-      alert(response.statusText);
-    }
-  }
-};
 
 document
   .querySelector('.login-form')
   .addEventListener('submit', loginFormHandler);
 
-document
-  .querySelector('.signup-form')
-  .addEventListener('submit', signupFormHandler);
---
+++ public/js/signup.js
@@ -0,0 +1,29 @@
+const signupFormHandler = async (event) => {
+    event.preventDefault();
+
+    const user_name = document.querySelector('#user-signup').value.trim();
+    const password = document.querySelector('#password-signup').value.trim();
+
+    if (user_name && password) {
+        const response = await fetch('/api/users', {
+            method: 'POST',
+            body: JSON.stringify({
+                user_name,
+                password
+            }),
+            headers: {
+                'Content-Type': 'application/json'
+            },
+        });
+
+        if (response.ok) {
+            document.location.replace('/profile');
+        } else {
+            alert(response.statusText);
+        }
+    }
+};
+
+document
+    .querySelector('.signup-form')
+    .addEventListener('submit', signupFormHandler);
