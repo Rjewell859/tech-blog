49a265bb09e925a66db0e0cc814399f8ec75749d
Headwall Forest <headwallforest27@gmail.com>
2022-10-12 15:01:53 -0400

    Comments post succesfully, viewing not yet functional

--- models/index.js
+++ models/index.js
@@ -1,13 +1,27 @@
 const User = require('./User');
 const Blog = require('./Blog');
+const Comment = require('./Comment')
 
 User.hasMany(Blog, {
   foreignKey: 'user_id',
   onDelete: 'CASCADE'
 });
 
+Comment.belongsTo(User, {
+  foreignKey: 'user_id'
+
+})
+
 Blog.belongsTo(User, {
   foreignKey: 'user_id'
 });
 
-module.exports = { User, Blog };
+Blog.hasMany(Comment, {
+  foreignKey: 'blog_id',
+  onDelete: 'CASCADE'
+})
+
+
+
+
+module.exports = { User, Blog, Comment };
---
+++ public/js/blog.js
@@ -0,0 +1,48 @@
+const newFormHandler = async (event) => {
+    event.preventDefault();
+  
+    const title = document.querySelector('#comment-title').value.trim();
+    const content = document.querySelector('#comment-cont').value.trim();
+  
+    if (title  && content) {
+        event.preventDefault()
+      const response = await fetch(`/api/comments`, {
+        method: 'POST',
+        body: JSON.stringify({ title,  content }),
+        headers: {
+          'Content-Type': 'application/json',
+        },
+      });
+  
+      if (response.ok) {
+        document.location.replace(`/blog/${blog_id}`);
+      } else {
+        alert('Failed to create comment');
+      }
+    }
+  };
+  
+  const delButtonHandler = async (event) => {
+    if (event.target.hasAttribute('data-id')) {
+      const id = event.target.getAttribute('data-id');
+  
+      const response = await fetch(`/api/comments/${id}`, {
+        method: 'DELETE',
+      });
+  
+      if (response.ok) {
+        document.location.replace('/profile');
+      } else {
+        alert('Failed to delete comment');
+      }
+    }
+  };
+  
+  document
+    .querySelector('.new-comment-form')
+    .addEventListener('submit', newFormHandler);
+  
+  document
+    .querySelector('.comment-list')
+    .addEventListener('click', delButtonHandler);
+  
