09025b9ab348f34541bbbdf888c487c8161ed367
Headwall Forest <headwallforest27@gmail.com>
2022-10-13 17:38:33 -0400

    Working on update functionality

--- controllers/homeRoutes.js
+++ controllers/homeRoutes.js
@@ -39,7 +39,7 @@ router.get('/blog/:id', async (req, res) => {
     });
 
     const blog = blogData.get({ plain: true });
-
+   
     res.render('blog', {
       ...blog,
       logged_in: req.session.logged_in
